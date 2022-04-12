dd3cbfe4d81a636556ff3144c7fbfa62f219373f
bialoian <ian.bialo@utt.fr>
2022-04-12 14:34:33 +0200

    Add email

--- pipeline.groovy
+++ pipeline.groovy
@@ -28,6 +28,14 @@ pipeline {
                     junit '**/target/surefire-reports/TEST-*.xml'
                     archiveArtifacts 'target/*.jar'
                 }
+                changed {
+                    emailext subject: "Job \'${JOB_NAME}\' (build ${BUILD_NUMBER}) ${currentBuild.result}",
+                        body: "Please go to ${BUILD_URL} and verify the build", 
+                        attachLog: true, 
+                        compressLog: true, 
+                        to: "test@test.fr",
+                        recipientProviders: [upstreamDevelopers(), requestor()]
+                }
             }
         }
     }
